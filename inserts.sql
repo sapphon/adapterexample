d7c5fadd780f280a2633259aaa9e59cf9f1fad15
unknown <sapphon@gmail.com>
2019-10-08 01:04:33 -0400

    Inserts for the demo

---
+++ inserts.sql
@@ -0,0 +1,5 @@
+insert into user values(1, FALSE, 'VP', 'Alvin', 9001);
+insert into user values(2, FALSE, 'Senior Theorist', 'Simon', 8999);
+insert into user values(3, TRUE, 'Secretary', 'Theodore', 9);
+insert into orange values(1, 10000, 5.0, 'Golden', 'Navel');
+insert into apple values(1, 500, 5000, 'Fire Engine', 'Gala');
